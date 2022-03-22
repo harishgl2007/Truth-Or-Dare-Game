ce8d5e99e823dffaa3e37f76813e72488ed43629
HARISH <67889880+harishgl2007@users.noreply.github.com>
2022-03-22 19:22:49 +0530

    python
    Made with Python

---
+++ Truth or Dare.py
@@ -0,0 +1,12 @@
+####Harishdev####
+import random
+
+names = ["Harish", "name1", "name2", "name3", "name4", "name5"]
+state = ["Truth", "Dare"]
+
+namemain = random.choice(names)
+namemain2 = random.choice(names)
+statemain = random.choice(state)
+
+print(namemain, "Should Give", statemain, "To", namemain2)
+
